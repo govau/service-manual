aa3a39dcfdf51fd4b78002e84f4ad0cb1f4f5e6f
Simon Schwartz <simon.schwartz0@gmail.com>
2017-05-09 11:04:14 +1000

    remove footer

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -6,7 +6,6 @@ import Container from "./components/Container"
 import DefaultHeadMeta from "./components/DefaultHeadMeta"
 import Header from "./components/Header"
 import Content from "./components/Content"
-import Footer from "./components/Footer"
 
 const AppContainer = (props) => (
   <Container>
@@ -15,7 +14,6 @@ const AppContainer = (props) => (
     <Content>
       { props.children }
     </Content>
-    <Footer />
   </Container>
 )
 
