9c166e485c8c90b88b3d4c5dc91a81d93e4b5e99
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-06-14 12:27:55 +1000

    added 404 page
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

---
+++ src/components/404.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default ( page ) => (
+	<div className="uikit-body">
+		<div>{ page._body }</div>
+		<ul>
+			{
+				Object.keys( page._sites ).map( ( site, i ) => {
+					if( site !== '404' ) {
+						return (
+							<li key={ i }>
+								<a href={ `${ page._sites[ site ].url }` }>{ page._sites[ site ].title }</a>
+							</li>
+						)
+					}
+				})
+			}
+		</ul>
+	</div>
+);
