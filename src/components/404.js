9b921d038086c08b158e03cde6593ad16f87098a
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-06-18 10:35:19 +1000

    fixed up 404 page
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/components/404.js
+++ src/components/404.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 export default ( page ) => (
-	<div className="uikit-body">
-		<div>{ page._body }</div>
-		<ul>
-			{
-				Object.keys( page._sites ).map( ( site, i ) => {
-					if( site !== '404' ) {
-						return (
-							<li key={ i }>
-								<a href={ `${ page._sites[ site ].url }` }>{ page._sites[ site ].title }</a>
-							</li>
-						)
-					}
-				})
-			}
-		</ul>
+	<div className="uikit-body container page404">
+		<div className="textwrapper">
+			<div className="page404__text">{ page._body }</div>
+			<ul className="page404__list">
+				{
+					Object.keys( page._sites ).map( ( site, i ) => {
+						if( site !== '404' ) {
+							return (
+								<li key={ i }>
+									<a href={ `${ page._sites[ site ].url }` }>{ page._sites[ site ].title }</a>
+								</li>
+							)
+						}
+					})
+				}
+			</ul>
+		</div>
 	</div>
 );
