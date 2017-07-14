c65af8a2f8b6a252c88bdcbbdb8f8e9746d8bea7
Mark <ma>
2017-07-14 15:22:50 +1000

    feedback component

---
+++ src/components/feedback.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+
+/**
+ * The Feedback component to be used after all content blocks
+ *
+ * @disable-docs
+ */
+const Feedback = ( page ) => (
+	<div className="uikit-body uikit-grid feedback">
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-12">
+					<span className="feedback__caption">{ page.caption }</span>
+					<div>
+						<a href={ page.buttonUrl } ><button className="uikit-btn">{ page.buttonText }</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+
+Feedback.propTypes = {
+
+	/**
+	 * caption: This is the first release of Gov.au Service Manual. We’d love to know what’s missing or could be done better
+	 */
+	caption: PropTypes.string.isRequired,
+
+	/**
+	 * buttonUrl: /feedback
+	 */
+	buttonUrl: PropTypes.string.isRequired,
+
+	/**
+	 * buttonText: Send us feedback
+	 */
+	buttonText: PropTypes.string.isRequired,
+};
+
+
+Feedback.defaultProps = {};
+
+
+export default Feedback;
