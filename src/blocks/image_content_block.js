5123bb7ef37fefae7252ed910b5b68b95d930083
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-07-19 16:12:53 +1000

    made alt text be required
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- src/blocks/image_content_block.js
+++ src/blocks/image_content_block.js
@@ -70,7 +70,7 @@ ImageContentblock.propTypes = {
 	/**
 	 * imageAlt: this is an image
 	 */
-	imageAlt: PropTypes.string,
+	imageAlt: PropTypes.string.isRequired,
 
 	/**
 	 * caption: An example of an affinity mapping session in action.
@@ -91,7 +91,6 @@ ImageContentblock.propTypes = {
 
 ImageContentblock.defaultProps = {
 	level: 2,
-	imageAlt: '',
 };
 
 
