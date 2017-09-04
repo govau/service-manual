06772775eafa6831849ff5f930667aa7fe89f761
Mark <ma>
2017-09-04 10:24:59 +1000

    added id to intro section

--- src/intros/intro.js
+++ src/intros/intro.js
@@ -15,7 +15,7 @@ const Intro = ( page ) => {
 				<div className="row">
 					<div className="col-md-12">
 						<div className="textwrapper">
-							{ page.category && <span className="section__section intro__category">{ page.category }</span> }
+							{ page.category && <span id={ (page.category).toLowerCase() } className="section__section intro__category">{ page.category }</span> }
 							<h1 className="intro__headline">{ page.title ? page.title : page._pages[ page._ID ].title }</h1>
 							{ page.subtitle && <p className="intro__subtitle">{ page.subtitle }</p> }
 							<div className="intro__text">{ page._body }</div>
@@ -55,4 +55,3 @@ Intro.defaultProps = {};
 
 
 export default Intro;
-
