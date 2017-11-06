9f74a39d4dc531ab2ac0f12a9a0cfe4a45791f89
Mark <ma>
2017-11-06 14:08:46 +1100

    added meta description to topic pages

--- src/layout/topic.js
+++ src/layout/topic.js
@@ -13,6 +13,7 @@ const Topic = ( page ) => (
 		<meta charSet="utf-8" />
 		<meta http-equiv="x-ua-compatible" content="ie=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content={ page.description } />
 
 		<link rel="stylesheet" href={ `/assets/css/site.css` } />
 		{ page.stylesheet != undefined
