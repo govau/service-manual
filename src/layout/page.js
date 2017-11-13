adf16e142d54968e3ff458edacfe12abf7f415f9
Mark <ma>
2017-11-13 11:16:16 +1100

    google site verification

--- src/layout/page.js
+++ src/layout/page.js
@@ -10,6 +10,7 @@ const Page = ( page ) => (
 	<head>
 		<title>Guides - { page.title }</title>
 		<meta charSet="utf-8" />
+		<meta name="google-site-verification" content="fXPmqCfxWVHNxpSlw6Bm6pmuUcMuTILat3eixX8gbM8" />
 		<meta http-equiv="x-ua-compatible" content="ie=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content={ page.description }/>
