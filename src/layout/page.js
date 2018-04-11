37f50e9696849f50b32279e56340950167a17c4a
Chris Lee <sirleech@gmail.com>
2018-04-11 11:03:30 +1000

    update page title to Digital Guides

--- src/layout/page.js
+++ src/layout/page.js
@@ -8,7 +8,7 @@ import SkipLink from '../../scripts/uikit/skip-link.js';
 const Page = ( page ) => (
 	<html>
 	<head>
-		<title>Guides - { page.pagetitle }</title>
+		<title>Digital Guides - { page.pagetitle }</title>
 		<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png" />
 		<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png" />
 		<meta charSet="utf-8" />
