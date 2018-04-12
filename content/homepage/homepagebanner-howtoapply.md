04cfc304e117062c175587e4bc399fb2a05135a4
Chris Lee <sirleech@gmail.com>
2018-04-13 09:25:35 +1000

    add the second homepage banner content

---
+++ content/homepage/homepagebanner-howtoapply.md
@@ -0,0 +1,10 @@
+---
+layout: text/homepagebanner
+button_text: View Topics
+button_link: /how-to-apply-the-standard/
+oddrow: true
+heading: How to apply the Standard
+---
+Learn how to apply the Standard to your digital services. This section covers
+how to build an agile service team, doing user research, and designing and 
+delivering a service.
--- src/text/homepagebanner.js
+++ src/text/homepagebanner.js
@@ -11,10 +11,12 @@ const HomepageBanner = ( page ) => {
 
 	return (
 		<div className={"container-fluid homepagebanner " + (page.oddrow ? 'homepagebanner__oddrow' : '')}>
-			<div class="row">
-				<div class="col-xs-12">
+			<div className="row">
+				<div className="col-xs-12">
 					<h2>{ page.heading }</h2>
-					{ page._body }
+					<div className="homepagebanner_paragraph">
+						{ page._body }
+					</div>
 					<a href={ page.button_link }>{ page.button_text }</a>
 				</div>
 			</div>
