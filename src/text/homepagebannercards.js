d32377ced326f86a88a822de62166c814f9bb6e5
Alex Page <alex@alexpage.com.au>
2018-04-19 15:07:11 +1000

    Adjusting cards on homepage to matchheight

--- src/text/homepagebannercards.js
+++ src/text/homepagebannercards.js
@@ -11,52 +11,53 @@ import Card from '../cards/card';
 const HomepageBannerCards = ( page ) => {
 
 	return (
-		<div className={"au-body homepagebanner__lastrow container-fluid homepagebanner " + (page.oddrow ? 'homepagebanner__oddrow' : '')}>
+		<div className={"au-body homepagebanner__lastrow homepagebanner " + (page.oddrow ? 'homepagebanner__oddrow' : '')}>
 
-			<div className="row">
-				<div className="col-xs-12 homepagebanner__container">
-					<h2 className="au-display-xxxl">{ page.heading }</h2>
-					<div className="homepagebanner__paragraphcontainer">
-						{ page._body }
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-12 homepagebanner__container">
+						<h2 className="au-display-xxxl">{ page.heading }</h2>
+						<div className="homepagebanner__paragraphcontainer">
+							{ page._body }
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="row">
-				<div className="col-xs-12 homepagebannercards__container">
-					<div className="homepagebannercard__container">
-						<Card
-							link="/content-strategy/"
-							background=""
-							image=""
-							headline="Content Strategy Guide"
-							text="Content strategy comes before you design your content. This sets the foundation to create, deliver and govern content."
-							cta=" "
-						/>
-					</div>
-					<div className="homepagebannercard__container">
-						<Card
-							link="https://guides.service.gov.au/content-guide/"
-							background=""
-							image=""
-							headline="Content Guide"
-							text="A guide to help Australian Government teams design simple, clear and fast content."
-							cta=" "
-						/>
-					</div>
-					<div className="homepagebannercard__container">
-						<Card
-							link="https://designsystem.gov.au/"
-							background=""
-							image=""
-							headline="Design System"
-							text="A framework and set of tools to help build accessible products and services."
-							cta=" "
-						/>
-					</div>
+				<div className="row au-grids cards">
+					<ul className="cards__list">
+						<li className="col-sm-4 cards__list__item">
+							<Card
+								link="/content-strategy/"
+								background=""
+								image=""
+								headline="Content Strategy Guide"
+								text="Content strategy comes before you design your content. This sets the foundation to create, deliver and govern content."
+								cta=" "
+							/>
+						</li>
+						<li className="col-sm-4 cards__list__item">
+							<Card
+								link="https://guides.service.gov.au/content-guide/"
+								background=""
+								image=""
+								headline="Content Guide"
+								text="A guide to help Australian Government teams design simple, clear and fast content."
+								cta=" "
+							/>
+						</li>
+						<li className="col-sm-4 cards__list__item">
+							<Card
+								link="https://designsystem.gov.au/"
+								background=""
+								image=""
+								headline="Design System"
+								text="A framework and set of tools to help build accessible products and services."
+								cta=" "
+							/>
+						</li>
+					</ul>
 				</div>
 			</div>
-
 		</div>
 	)};
 
