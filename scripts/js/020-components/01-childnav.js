737f08cb0dcf91833e0c9cbd4c9b5959a01e6025
Chris Lee <sirleech@gmail.com>
2018-05-04 11:00:59 +1000

    better comments

--- scripts/js/020-components/01-childnav.js
+++ scripts/js/020-components/01-childnav.js
@@ -1,3 +1,8 @@
+// these are the event handlers for the ChildNav accordion
+// What does it do?
+// 1) responds to media query changes, page resizes to compact the accordion
+// 2) adds a click handler to the menu button that appears in XS viewports.
+
 var childNavElement  = document.getElementById( 'guides-childnav-accordion' );
 var childNavClicked = false;
 var childNavButton = document.getElementById('childnav__button');
