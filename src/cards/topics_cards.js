77476730b36e389b9abfc70051189f81dde104bd
Chris Lee <sirleech@gmail.com>
2018-05-21 11:05:37 +1000

    Add a skeleton topics cards component based off of homepagecards

---
+++ src/cards/topics_cards.js
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types';
+import Slugify from 'slugify';
+import React from 'react';
+import Card from '../cards/card';
+
+
+/**
+ * The Topics Cards component
+ */
+
+const TopicsCards = ( page ) => {
+
+	return (
+		<div className={"au-body homepagebanner__lastrow homepagebanner " + (page.oddrow ? 'homepagebanner__oddrow' : '')}>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-12 homepagebanner__container">
+						<h2 className="au-display-xxxl">{ page.heading }</h2>
+						<div className="homepagebanner__paragraphcontainer">
+							{ page._body }
+						</div>
+					</div>
+				</div>
+
+				<div className="row au-grids cards guideshomepagecards">
+					<ul className="cards__list">
+						<li className="col-sm-4 cards__list__item">
+							<Card
+								link= { page.card1link }
+								background=""
+								image=""
+								headline= { page.card1headline }
+								text= { page.card1text }
+								cta=" "
+							/>
+						</li>
+						<li className="col-sm-4 cards__list__item">
+							<Card
+								link= { page.card2link }
+								background=""
+								image=""
+								headline= { page.card2headline }
+								text= { page.card2text }
+								cta=" "
+							/>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)};
+
+
+TopicsCards.propTypes = {
+
+	/**
+	 * _body: (text)(7)
+	 */
+	_body: PropTypes.node.isRequired,
+};
+
+
+TopicsCards.defaultProps = {};
+
+
+export default TopicsCards;
