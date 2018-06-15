bbfe779fd20b94df590f78ab265e7a5e51af35fc
Chris Lee <sirleech@gmail.com>
2018-06-15 14:25:29 +1000

    add empty script for building the lunr search index

---
+++ scripts/createsearchindex.js
@@ -0,0 +1,7 @@
+console.log("=================================");
+console.log("Creating search index...");
+console.log("=================================");
+
+// Scan through all content/ directories
+//   For each markdown file
+//     CreateIndex(title,content,path)
