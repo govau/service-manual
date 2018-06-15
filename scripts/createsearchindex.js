728191f60230f4d84ea8b43bc30c015a0f8229a9
Chris Lee <sirleech@gmail.com>
2018-06-15 14:42:32 +1000

    install Lunr and run a basic search

--- scripts/createsearchindex.js
+++ scripts/createsearchindex.js
@@ -5,3 +5,27 @@ console.log("=================================");
 // Scan through all content/ directories
 //   For each markdown file
 //     CreateIndex(title,content,path)
+
+var lunr = require('lunr');
+
+var idx = lunr(function () {
+  this.field('title')
+  this.field('body')
+
+  this.add({
+    "title": "Twelfth-Night",
+    "body": "If music be the food of love, play on: Give me excess of it…",
+    "author": "William Shakespeare",
+    "id": "/william/shakespeare/"
+  })
+	this.add({
+		"title": "Batman",
+		"body": "Batman loves throwing batarangs.",
+		"author": "DC Comics",
+		"id": "/dc/batman/"
+	})
+})
+
+console.log(idx.search("love"));
+console.log("=================================");
+console.log(JSON.stringify(idx));
