74dff199966b98d785a976e91f1165ffc7329209
Chris Lee <sirleech@gmail.com>
2018-06-18 14:54:45 +1000

    Get document data from file system

--- scripts/createsearchindex.js
+++ scripts/createsearchindex.js
@@ -9,46 +9,26 @@ console.log("      Creating Lunr index...");
 const lunr = require('lunr');
 const fs = require('fs');
 
-const documents = [
-{
-	"title": "Discovery stage: exploring the problem",
-	"body": "discovery stage service design delivery process kick-off",
-	"path": "/topics/service-design-delivery-process/discovery-stage/"
-},
-{
-	"title": "Roles",
-	"body": "roles team discovery culture service manager product manager designers developer",
-	"path": "/topics/starting-team/roles/"
-},
-{
-	"title": "Improve your SEO",
-	"body": "Search engine optimisation",
-	"path": "/content-strategy/search-engine-optimisation/"
-},
-{
-	"title": "Identify business needs",
-	"body": "content strategy align agreement",
-	"path": "/content-strategy/identify-business-needs/"
-}
-]
+fs.readFile('scripts/documents.json', function(err,data) {
+	const documents = JSON.parse(data);
 
-const index = lunr(function () {
-  this.field('title')
-  this.field('body')
-	this.field('path')
-	this.ref('path')
+	const index = lunr(function () {
+	  this.field('title')
+	  this.field('body')
+		this.field('path')
+		this.ref('path')
 
-	documents.forEach(function (doc) {
-    this.add(doc)
-  }, this)
+		documents.forEach(function (doc) {
+	    this.add(doc)
+	  }, this)
 
-})
+	})
 
-const serialisedIndex = JSON.stringify(index);
+	const serialisedIndex = JSON.stringify(index);
 
-fs.writeFile('site/search_index.json', serialisedIndex, (err) => {
-  if (err) throw err;
-  console.log('The Lunr search index has been created -> site/search_index.json');
+	fs.writeFile('site/search_index.json', serialisedIndex, (err) => {
+	  if (err) throw err;
+	  console.log('The Lunr search index has been created -> site/search_index.json');
+	});
+
 });
-
-console.log("=================================");
