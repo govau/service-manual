b113ec1dddc52f499c241e56fef5fa407571d0ba
Chris Lee <sirleech@gmail.com>
2018-06-18 14:29:19 +1000

    refactor

--- scripts/createsearchindex.js
+++ scripts/createsearchindex.js
@@ -9,32 +9,39 @@ console.log("      Creating Lunr index...");
 const lunr = require('lunr');
 const fs = require('fs');
 
-var index = lunr(function () {
+const documents = [
+{
+	"title": "Discovery stage: exploring the problem",
+	"body": "discovery stage service design delivery process kick-off",
+	"path": "/topics/service-design-delivery-process/discovery-stage/"
+},
+{
+	"title": "Roles",
+	"body": "roles team discovery culture service manager product manager designers developer",
+	"path": "/topics/starting-team/roles/"
+},
+{
+	"title": "Improve your SEO",
+	"body": "Search engine optimisation",
+	"path": "/content-strategy/search-engine-optimisation/"
+},
+{
+	"title": "Identify business needs",
+	"body": "content strategy align agreement",
+	"path": "/content-strategy/identify-business-needs/"
+}
+]
+
+const index = lunr(function () {
   this.field('title')
   this.field('body')
 	this.field('path')
 	this.ref('path')
 
-  this.add({
-    "title": "Discovery stage: exploring the problem",
-    "body": "discovery stage service design delivery process kick-off",
-    "path": "/topics/service-design-delivery-process/discovery-stage/"
-  })
-	this.add({
-		"title": "Roles",
-		"body": "roles team discovery culture service manager product manager designers developer",
-		"path": "/topics/starting-team/roles/"
-	})
-	this.add({
-		"title": "Identify business needs",
-		"body": "content strategy align agreement",
-		"path": "/content-strategy/identify-business-needs/"
-	})
-	this.add({
-		"title": "SEO",
-		"body": "Search engine optimisation",
-		"path": "/content-strategy/search-engine-optimsation/"
-	})
+	documents.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+
 })
 
 const serialisedIndex = JSON.stringify(index);
