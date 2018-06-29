ed73ba5b70b3800e9a8749c718e6c85c55a48d6f
Chris Lee <sirleech@gmail.com>
2018-06-29 15:10:00 +1000

    Ask for response on "Yes"

--- scripts/js/020-components/05-feedback.js
+++ scripts/js/020-components/05-feedback.js
@@ -1,16 +1,23 @@
 var helpful_yes = document.querySelector( '.helpful_yes' );
 var helpful_no = document.querySelector( '.helpful_no' );
 var helpful_text = document.querySelector( '.helpful__question' );
+var helpful__yes__textarea = document.querySelector( '.helpful__yes__textarea' );
+var helpful__yes__suggestion__submit = document.querySelector( '.helpful__yes__suggestion__submit' );
 var helpful__no__textarea = document.querySelector( '.helpful__no__textarea' );
 var helpful__no__suggestion__submit = document.querySelector( '.helpful__no__suggestion__submit' );
 
+
+helpful__yes__textarea.style.display = "none";
+helpful__yes__suggestion__submit.style.display = "none";
 helpful__no__textarea.style.display = "none";
 helpful__no__suggestion__submit.style.display = "none";
 
 function thanksYes(){
-	helpful_text.innerHTML = "Thank you for your response, it helps us improve our service.";
+	helpful_text.innerHTML = "Thank you. Let us know more!";
 	helpful_no.remove();
 	helpful_yes.remove();
+	helpful__yes__textarea.style.display = "inline";
+	helpful__yes__suggestion__submit.style.display = "inline";
 }
 
 function thanksNo(){
@@ -25,6 +32,8 @@ function thanksForFeedback(){
 	helpful_text.innerHTML = "Thank you for your response, it helps us improve our service.";
 	helpful__no__textarea.remove();
 	helpful__no__suggestion__submit.remove();
+	helpful__yes__textarea.remove();
+	helpful__yes__suggestion__submit.remove();
 }
 
 AddEvent( helpful_yes, 'click', function( event, $this ) {
@@ -48,6 +57,17 @@ AddEvent( helpful_no, 'click', function( event, $this ) {
 		eventAction: window.location.href,
 		eventLabel: 'helpful: no',
 		eventValue: -1
+	});
+});
+
+AddEvent( helpful__yes__suggestion__submit, 'click', function( event, $this ) {
+	PreventEvent( event );
+	thanksForFeedback();
+	ga('send', {
+		hitType: 'event',
+		eventCategory: 'helpful: yes: feedback response',
+		eventAction: window.location.href,
+		eventLabel: helpful__yes__textarea.value
 	});
 });
 
--- src/blocks/feedback.js
+++ src/blocks/feedback.js
@@ -18,8 +18,14 @@ const Feedback = ( page ) => {
 							<button className="au-btn helpful_yes">Yes</button>
 							<button className="au-btn helpful_no">No</button>
 						</span>
-						<div><textarea placeholder="Hi, I think we can improve..." className="helpful__no__textarea" rows="3" cols="30" /></div>
+						<div>
+							<textarea placeholder="Please type your suggestion or feedback here" className="helpful__yes__textarea" rows="3" cols="30" />
+						</div>
+						<div>
+							<textarea placeholder="Please type your suggestion or feedback here" className="helpful__no__textarea" rows="3" cols="30" />
+						</div>
 						<span className="helpful__buttons">
+							<button className="au-btn helpful__yes__suggestion__submit">Submit</button>
 							<button className="au-btn helpful__no__suggestion__submit">Submit</button>
 						</span>
 					</div>
