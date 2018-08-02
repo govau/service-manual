2d1d52cc1dc2bf5d3fea802fddad6d9108ca3197
Chris Lee <sirleech@gmail.com>
2018-08-02 13:53:43 +1000

    Use the correct COA logo and placement.

--- src/structure/header.js
+++ src/structure/header.js
@@ -39,7 +39,7 @@ const Header = ({ title, title_badge, mainmenu, header_govau, _relativeURL, _ID,
 				    <div className="row">
 				      <div className="col-md-7">
 								<div className="header__brand--print">
-									<img src="/assets/img/coa.png"/><strong>Digital Guides</strong>
+									<img src="/assets/img/coa-black.png"/><strong>Digital Guides</strong>
 								</div>
 								<AUheaderBrand
 					          title="Digital Guides"
