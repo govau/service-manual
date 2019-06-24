45cf60acd905fa2abc1e966423c9fedc191692cb
Michael Tran <michael@dublue.com>
2019-06-24 16:28:31 +1000

    Redo redirection

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const Server = Express();
 var redirect_map = require('./redirect-map.json');
 Server.locals.redirect_map = redirect_map;
 for (var key in redirect_map) {
+	if (key.slice(-1) == '/')
+		key += '?';
+	else
+		key += '/?';
+
 	Server.get(key, (req, res) => {
 		res.redirect(301, Server.locals.redirect_map[req.path]);
 	});
